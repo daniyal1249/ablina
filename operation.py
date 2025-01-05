70e28e8e247532576eeb6237810fcb38d21bacc2
Daniyal <daniyalakif@gmail.com>
2025-01-05 06:16:23 -0600

    add dunder eq method

--- operation.py
+++ operation.py
@@ -1,3 +1,6 @@
+from numbers import Real
+
+import sympy as sp
 from utils import of_arity
 
 class OperationError(Exception):
@@ -18,31 +21,73 @@ class Operation:
     @property
     def arity(self):
         return self._arity
-
+    
+    def __call__(self, *args):
+        return self.func(*args)
 
 
 class VectorAdd(Operation):
-    def __init__(self, func, n):
+    def __init__(self, field, n, func):
         super().__init__(func, 2)
+        self._field = field
         self._n = n
+
+    @property
+    def field(self):
+        return self._field
 
     @property
     def n(self):
         return self._n
     
     def __eq__(self, add2):
-        pass
+        if add2 is self:
+            return True
+        try:
+            # Initialize two arbitrary vectors (xs and ys)
+            if self.field is Real:
+                xs, ys = sp.symbols((f'x:{self.n}', f'y:{self.n}'), real=True)
+            else:
+                xs, ys = sp.symbols((f'x:{self.n}', f'y:{self.n}'))
+
+            xs, ys = list(xs), list(ys)
+            for lhs, rhs in zip(self.func(xs, ys), add2.func(xs, ys)):
+                if not sp.sympify(lhs).equals(sp.sympify(rhs)):
+                    return False
+            return True
+        except Exception:
+            return None
 
 
 class ScalarMul(Operation):
-    def __init__(self, func, n):
+    def __init__(self, field, n, func):
         super().__init__(func, 2)
+        self._field = field
         self._n = n
+
+    @property
+    def field(self):
+        return self._field
 
     @property
     def n(self):
         return self._n
     
     def __eq__(self, mul2):
-        pass
+        if mul2 is self:
+            return True
+        try:
+            # Initialize an arbitrary vector (xs) and scalar (c)
+            if self.field is Real:
+                xs, c = sp.symbols((f'x:{self.n}', 'c'), real=True)
+            else:
+                xs, c = sp.symbols((f'x:{self.n}', 'c'))
+
+            xs = list(xs)
+            for lhs, rhs in zip(self.func(c, xs), mul2.func(c, xs)):
+                if not sp.sympify(lhs).equals(sp.sympify(rhs)):
+                    return False
+            return True
+        except Exception:
+            return None
 
