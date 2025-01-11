5943160aeee5300f72198c0a98c73efc53a0a7b4
Daniyal <daniyalakif@gmail.com>
2025-01-10 18:56:39 -0600

    add MathematicalSet to imports

--- alapy/__init__.py
+++ alapy/__init__.py
@@ -1,6 +1,6 @@
 from alapy.innerproduct import InnerProductSpace
 from alapy.linearmap import LinearMap, Isomorphism, IdentityMap
-from alapy.mathset import Set
+from alapy.mathset import MathematicalSet, Set
 from alapy.vectorspace import (
     VectorSpace, Fn, is_vectorspace, columnspace, rowspace, nullspace, 
     left_nullspace, image, kernel)
