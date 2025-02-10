7b0784c0111fe870519680797bc16ebb46fe8b5c
Daniyal <daniyalakif@gmail.com>
2025-02-09 19:55:08 -0600

    add metadata

--- alapy/__init__.py
+++ alapy/__init__.py
@@ -1,8 +1,27 @@
+__version__ = '0.1.0'
+__author__ = 'Daniyal Akif'
+__email__ = 'daniyalakif@gmail.com'
+__license__ = 'MIT'
+__description__ = 'A Python package for abstract linear algebra.'
+__url__ = 'https://github.com/daniyal1249/alapy'
+
+
 from numbers import Complex, Real
 
-from alapy.innerproduct import InnerProductSpace
-from alapy.linearmap import LinearMap, Isomorphism, IdentityMap
-from alapy.mathset import MathematicalSet, Set
-from alapy.vectorspace import (
-    VectorSpace, Fn, is_vectorspace, columnspace, rowspace, nullspace, 
-    left_nullspace, image, kernel)
+from .innerproduct import InnerProductSpace
+from .linearmap import LinearMap, Isomorphism, IdentityMap
+from .mathset import MathematicalSet, Set
+from .vectorspace import (
+    VectorSpace, Fn, is_vectorspace, columnspace, rowspace, 
+    nullspace, left_nullspace, image, kernel
+)
+
+
+__all__ = [
+    'Complex', 'Real',
+    'InnerProductSpace',
+    'LinearMap', 'Isomorphism', 'IdentityMap',
+    'MathematicalSet', 'Set',
+    'VectorSpace', 'Fn', 'is_vectorspace', 'columnspace', 'rowspace',
+    'nullspace', 'left_nullspace', 'image', 'kernel'
+]
