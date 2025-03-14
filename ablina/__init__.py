e7d09e67169caa1361b0b395d4c4ca920b262df0
Daniyal <daniyalakif@gmail.com>
2025-03-13 21:09:17 -0500

    version 0.3.0

--- ablina/__init__.py
+++ ablina/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 __author__ = 'Daniyal Akif'
 __email__ = 'daniyalakif@gmail.com'
 __license__ = 'MIT'
@@ -12,16 +12,17 @@ from .innerproduct import InnerProductSpace
 from .linearmap import LinearMap, Isomorphism, IdentityMap
 from .mathset import MathematicalSet, Set
 from .vectorspace import (
-    VectorSpace, Fn, is_vectorspace, columnspace, rowspace, 
-    nullspace, left_nullspace, image, kernel
+    VectorSpace, Fn, AffineSpace, fn, matrix_space, poly_space, hom, 
+    is_vectorspace, columnspace, rowspace, nullspace, left_nullspace, 
+    image, kernel
     )
-
 
 __all__ = [
     'Complex', 'Real',
     'InnerProductSpace',
     'LinearMap', 'Isomorphism', 'IdentityMap',
     'MathematicalSet', 'Set',
-    'VectorSpace', 'Fn', 'is_vectorspace', 'columnspace', 'rowspace',
-    'nullspace', 'left_nullspace', 'image', 'kernel'
+    'VectorSpace', 'Fn', 'AffineSpace', 'fn', 'matrix_space', 'poly_space',
+    'hom', 'is_vectorspace', 'columnspace', 'rowspace', 'nullspace',
+    'left_nullspace', 'image', 'kernel'
     ]
