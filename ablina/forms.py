2d3d3b55bc903dca4071c59c163d51f799b927e8
Daniyal <daniyalakif@gmail.com>
2025-04-30 02:35:28 -0500

    add inertia/signature methods and fix definiteness checks

--- ablina/forms.py
+++ ablina/forms.py
@@ -5,8 +5,16 @@ import sympy as sp
 from .utils import is_invertible, of_arity
 from .vectorspace import VectorSpace
 
+# Note that methods/properties such as is_positive_definite 
+# will return None if the matrix is symbolic
+
 
 class FormError(Exception):
+    def __init__(self, msg=''):
+        super().__init__(msg)
+
+
+class InnerProductError(Exception):
     def __init__(self, msg=''):
         super().__init__(msg)
 
@@ -70,9 +78,9 @@ class SesquilinearForm:
         return sp.Matrix(n, n, lambda i, j: mapping(basis[i], basis[j]))
 
     @staticmethod
-    def _from_matrix(vectorspace: VectorSpace, matrix):
+    def _from_matrix(vectorspace, matrix):
         matrix = sp.Matrix(matrix)
-        def to_coord(vec): return sp.Matrix(vectorspace.to_coordinate(vec))
+        def to_coord(v): return sp.Matrix(vectorspace.to_coordinate(v))
         return lambda u, v: (to_coord(u).H @ matrix @ to_coord(v))[0]
 
     @property
@@ -106,28 +114,35 @@ class SesquilinearForm:
     def __str__(self):
         return self.__repr__()
 
-    def __eq__(self, map2):
-        if not isinstance(map2, SesquilinearForm):
+    def __eq__(self, form2):
+        if not isinstance(form2, SesquilinearForm):
             return NotImplemented
         return (
-            self.vectorspace == map2.vectorspace 
-            and self.matrix == map2.matrix
+            self.vectorspace == form2.vectorspace 
+            and self.matrix == form2.matrix
             )
     
     def __call__(self, vec1, vec2):
-        """
-        pass
-        """
         if vec1 not in self.vectorspace or vec2 not in self.vectorspace:
             raise TypeError(f'Vectors must be elements of the vector space.')
         return self.mapping(vec1, vec2)
-    
-    def signature(self):
-        pass
 
     def inertia(self):
-        pass
+        if self.vectorspace.field is Real:
+            if not self.is_symmetric():
+                raise FormError()
+        elif not self.is_hermitian():
+            raise FormError()
+        eigenvals = self.matrix.evalf().eigenvals().items()
+        p = sum(m for val, m in eigenvals if val > 0)
+        m = sum(m for val, m in eigenvals if val < 0)
+        z = sum(m for val, m in eigenvals if abs(val) < 1e-8)
+        return p, m, z
     
+    def signature(self):
+        p, m, _ = self.inertia()
+        return p - m
+
     def is_degenerate(self):
         return not is_invertible(self.matrix)
     
@@ -136,24 +151,73 @@ class SesquilinearForm:
 
     def is_hermitian(self):
         if self.vectorspace.field is not Complex:
-            raise TypeError()
+            raise FormError()
         return self.matrix.is_hermitian
 
     def is_positive_definite(self):
+        if not self.matrix.is_hermitian:
+            return False
         return self.matrix.is_positive_definite
 
     def is_negative_definite(self):
+        if not self.matrix.is_hermitian:
+            return False
         return self.matrix.is_negative_definite
 
     def is_positive_semidefinite(self):
+        if not self.matrix.is_hermitian:
+            return False
         return self.matrix.is_positive_semidefinite
 
     def is_negative_semidefinite(self):
+        if not self.matrix.is_hermitian:
+            return False
         return self.matrix.is_negative_semidefinite
+
+    def is_indefinite(self):
+        if not self.matrix.is_hermitian:
+            return False
+        return self.matrix.is_indefinite
 
 
 class InnerProduct(SesquilinearForm):
+    """
     pass
+    """
+
+    def __init__(self, vectorspace, mapping=None, matrix=None, name=None):
+        """
+        pass
+
+        Parameters
+        ----------
+        vectorspace : VectorSpace
+            The vector space the inner product is defined on.
+        mapping : callable, optional
+            A function that takes two vectors in the vector space and 
+            returns a scalar in the field.
+        matrix : list of list or sympy.Matrix, optional
+            The matrix representation of the inner product with respect 
+            to the basis of the vector space.
+        name : str, optional
+            pass
+
+        Returns
+        -------
+        InnerProduct
+            pass
+
+        Raises
+        ------
+        FormError
+            If neither the mapping nor the matrix is provided.
+        InnerProductError
+            If 
+        """
+        super().__init__(vectorspace, mapping, matrix, name)
+
+        if not self.is_positive_definite():
+            raise InnerProductError('Form is not positive definite.')
 
 
 class QuadraticForm:
