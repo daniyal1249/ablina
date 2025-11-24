878ea07dc2cb07863b0f0340a8f00c5a93db02ba
Daniyal <daniyalakif@gmail.com>
2025-11-23 18:05:17 -0600

    ignore

--- ablina/visualize.py
+++ ablina/visualize.py
@@ -9,6 +9,3 @@ except ImportError:
         "Manim is required for the visualize module. Install it using: \n\n"
         "    pip install ablina[visualize]"
         )
-
-from .linearmap import LinearMap
-from .vectorspace import VectorSpace
